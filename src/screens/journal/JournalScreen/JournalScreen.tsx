f633a0c5178b7104f7ecc6be2af56b26eb3bf292
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-09-05 01:58:43 -0500

    ADD: se termino video 10

--- src/screens/journal/JournalScreen/JournalScreen.tsx
+++ src/screens/journal/JournalScreen/JournalScreen.tsx
@@ -1,11 +1,16 @@
+import Sidebar from "../Sidebar/Sidebar";
+
 interface MyProps {}
 const defaultProps = {};
 const JournalScreen = (props: MyProps) => {
   props = { ...defaultProps, ...props };
   const {} = props;
   return (
-    <div data-testid="JournalScreen" className="">
-      <h1>JournalScreen component</h1>
+    <div data-testid="JournalScreen" className="journal__main-content">
+      <Sidebar />
+      <main>
+        <h1>MainContent</h1>
+      </main>
     </div>
   );
 };
---
+++ src/screens/journal/Sidebar/Sidebar.tsx
@@ -0,0 +1,23 @@
+interface MyProps {}
+const defaultProps = {};
+const Sidebar = (props: MyProps) => {
+  props = { ...defaultProps, ...props };
+  const {} = props;
+  return (
+    <aside data-testid="Sidebar" className="journal__sidebar">
+      <div className="journal__sidebar_navbar">
+        <h3>
+          <i className="fas fa-moon fa-2"></i>
+          <span> Pepito!!</span>
+        </h3>
+        <button className="btn">Logout</button>
+      </div>
+      <div className="journal__new-entry">
+        <i className="fas fa-calendar-alt fa-5x"></i>
+        <p className="mt-5">New Entry</p>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
