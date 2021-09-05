fd06bca9d57654a98aa3a9754d5123459d10f163
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-09-05 01:17:40 -0500

    ADD: estilos terminados en loginscreen

--- src/screens/auth/LoginScreen/LoginScreen.tsx
+++ src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 interface MyProps {}
 const defaultProps = {};
 const LoginScreen = (props: MyProps) => {
@@ -5,38 +7,61 @@ const LoginScreen = (props: MyProps) => {
   const {} = props;
   return (
     <div data-testid="LoginScreen" className="">
-      <h1>LoginScreen component</h1>
+      <h1 className="auth__title">Login</h1>
+      <div className="auth__alert-error">
+        <small>Ocurrio un error en el formulario</small>
+      </div>
       <form action="">
-        <div className="form-group">
+        <div className="auth__form-group">
           <label html-for="">Email:</label>
           <input
             type="text"
-            className="form-control"
+            className="auth__input"
             name="email"
             id="email"
             aria-describedby="emailHelpId"
             placeholder="Insert to Email"
+            autoComplete="false"
           />
-          <small id="emailelpId" className="form-text text-muted">
+          <small id="emailelpId" className="auth__form-text ">
             Help text
           </small>
         </div>
-        <div className="form-group">
+        <div className="auth__form-group">
           <label html-for="">Password:</label>
           <input
             type="text"
-            className="form-control"
+            className="auth__input"
             name="password"
             id="password"
             aria-describedby="passwordHelpId"
             placeholder="Insert to Email"
+            autoComplete="false"
           />
-          <small id="passwordHelpId" className="form-text text-muted">
+          <small id="passwordHelpId" className="auth__form-text ">
             Help text
           </small>
         </div>
-        <button>Login</button>
-        google
+        <button type="submit" className="btn btn-primary btn-block">
+          Login
+        </button>
+        <hr />
+        <div className="auth__social_networks">
+          <p>Login with Social Networks</p>
+          <div className="google-btn">
+            <div className="google-icon-wrapper">
+              <img
+                className="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="google button"
+              />
+            </div>
+            <p className="btn-text">
+              <b>Sign in with google</b>
+            </p>
+          </div>
+        </div>
+        <Link to="/auth/register">Create New Account</Link>
       </form>
     </div>
   );
